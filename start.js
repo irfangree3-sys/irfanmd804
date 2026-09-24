14c6a3bf49167c56b70c6c5ba946484389d1c8d3
irfangree3-sys <irfangree3@gmail.com>
2026-09-24 17:19:02 +0500

    Create start.js

---
+++ start.js
@@ -0,0 +1,12 @@
+const config = require("./config");
+const settings = require("./settings");
+
+console.log(`
+╭━━━〔 🤖 ${config.botName} 〕━━━╮
+┃ ⚡ WhatsApp Multi-Device
+┃ 👑 Powered by ${config.ownerNumber}
+┃ 🔧 Prefix: ${settings.prefix}
+╰━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+`);
+
+console.log("Starting IRFAN MD BOT...");
